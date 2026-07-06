8f8a9f664904bfed8b9f95a53414734cf809dacf
Luis <luismanuelr245@gmail.com>
2026-07-06 16:44:19 -0600

    feat: add WearHistorialScreen with rotary input (.rotaryScrollable)

---
+++ wear/src/main/java/mx/utng/lmrr/wear/data/LecturaFCLocal.kt
@@ -0,0 +1,12 @@
+package mx.utng.lmrr.wear.data
+
+/**
+ * Versión local de LecturaFC para el módulo wear (sin Room).
+ * Se mantiene en memoria en SmartHealthRepository.
+ */
+data class LecturaFCLocal(
+    val id: Int,
+    val valorBpm: Int,
+    val hora: String,
+    val esNormal: Boolean = valorBpm in 60..100
+)
